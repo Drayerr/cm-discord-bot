bc4ed8e24b4de416ca06d8faad5a2a1e0dc494b4
Drayerr <gabrielgrv2015@outlook.com>
2021-09-22 14:51:21 -0300

    Initializing Typescript .

--- src/controllers/commandController.js
+++ src/controllers/commandController.js
@@ -1,12 +1,7 @@
-const banana = require("../commands/banana")
 const ping = require("../commands/ping")
 const onCallMembers = require("../commands/onCallMembers")
-const meia7 = require('../commands/meia7')
 const play = require("../commands/play")
 const summon = require("../commands/summon")
-const chablau = require("../commands/chablau")
-const demonarch = require("../commands/demonarch")
-const drayerr = require("../commands/drayerr")
 const leave = require("../commands/leave")
 const random = require("../commands/random")
 
@@ -29,29 +24,14 @@ module.exports = function (message) {
   if (command === "ping") {
     ping(message, args)
   }
-  if (command === "banana") {
-    banana(message)
-  }
   if (command === "play") {
     play(message, args)
   }
   if (command === "oncall") {
     onCallMembers(message)
   }
-  if (command === "2345meia78") {
-    meia7(message)
-  }
   if (command === "summon") {
     summon(message)
-  }
-  if (command === "chablau") {
-    chablau(message)
-  }
-  if (command === "demonarch") {
-    demonarch(message)
-  }
-  if (command === "drayerr") {
-    drayerr(message)
   }
   if (command === "leave") {
     leave(message)
